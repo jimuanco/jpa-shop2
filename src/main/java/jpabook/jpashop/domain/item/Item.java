51e3555b8a8a477427322d1d06a8441427e5bbb6
Jimoo An <jmdh1004@gmail.com>
2023-08-28 19:18:32 +0900

    Item Domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,29 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //도메인 주도 설계 -> 엔티티가 해결할 수 있는 것은 엔티티안에 비즈니스로직을 넣는다. (객체 지향적)
+    //데이터를 가지고 있는 곳에 비즈니스 메서드 -> 응집력
+    //setter를 넣는게 아니라 stockQuantity를 변경하려면 이 메서드를 가지고 변경
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
